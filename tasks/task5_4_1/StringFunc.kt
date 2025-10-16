1a7f5e8360d91fa7077265344f2fbee5f715a1a9
Malachi <gqxd5106@leeds.ac.uk>
2025-10-16 09:49:16 +0000

    done up to 5_4_2

--- tasks/task5_4_1/StringFunc.kt
+++ tasks/task5_4_1/StringFunc.kt
@@ -1,1 +1,12 @@
 // Task 5.4.1: string extension function
+fun String.tooLong() = this.length > 20 
+
+fun main(){
+    val n = "stackofthenightwasgreatandfun"
+    if(n.tooLong()){
+        print("$n is too long")
+    }
+    else{
+        print("$n is a good length")
+    }
+}
