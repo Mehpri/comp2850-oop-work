aef43144024eda69283a256e71eb1c3572ebfa07
Malachi <gqxd5106@leeds.ac.uk>
2025-11-05 17:43:09 +0000

    Doing portfolio

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,19 @@
 // Implement the six required functions here
+fun isValid(word: String): Boolean{
+    if (word.length == 5) {
+        return true
+    }
+    else{
+        println("Your word is not valid in Wordle")
+    }
+}
+
+fun readWordList(filename: String): MutableList<String>{
+    val words = mutableListOf<String>()              
+    words.addAll(File(filename).readLines())         
+    return words
+}
+
+fun pickRandomWord(words: MutableList<String>): String{
+    
+}
