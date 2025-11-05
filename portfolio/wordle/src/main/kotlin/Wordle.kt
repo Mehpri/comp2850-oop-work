5408f9bb38c20d0ca5cf37c05a27ef21f75dea15
Malachi <gqxd5106@leeds.ac.uk>
2025-11-05 19:13:33 +0000

    Only have main left to do

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,19 +1,60 @@
 // Implement the six required functions here
+//1
 fun isValid(word: String): Boolean{
     if (word.length == 5) {
         return true
     }
     else{
         println("Your word is not valid in Wordle")
+        return false
     }
 }
 
+//2
 fun readWordList(filename: String): MutableList<String>{
     val words = mutableListOf<String>()              
     words.addAll(File(filename).readLines())         
     return words
 }
 
+//3
 fun pickRandomWord(words: MutableList<String>): String{
-    
+    val target = words.random()
+    words.remove(target)
+    return target
+}
+
+//4
+fun obtainGuess(attempt: Int): String{
+    println("Please enter attempt number $attempt")
+    val guess = readln()
+    while (isValid(guess) == false){
+        println("Please enter in a valid guess")
+        guess = readln()
+    }
+}
+
+fun evaluateGuess(guess: String, target: String): List<Int>{
+    val matches = mutableListOf<Int>()
+    for (i in 0 until 5){
+        if (guess[i] == target[i]){
+            matches.add(1)
+        }
+        else{
+            matches.add(0)
+        }
+    }
+    return matches
+}
+
+//6
+fun displayGuess(guess: String, matches: List<Int>){
+    for (i in 0 until 5){
+        if (matches[i] == 1){
+            println(guess[i])
+        }
+        else{
+            println("?")
+        }
+    }
 }
--- portfolio/wordle/src/test/kotlin/WordleTest.kt
+++ portfolio/wordle/src/test/kotlin/WordleTest.kt
@@ -1,8 +1,43 @@
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldNotContain
 
 @Suppress("unused")
 class WordleTest : StringSpec({
-    // Write your tests here
+    "1: A 5 letter word should return the boolean value of true"{
+        isValid("bread") shouldBe true 
+    }
+    
+    "A word that doesn't have 5 letters should return false"{
+        isValid("peas") shouldBe false
+    }
+
+    "2: All the words should be read from words.txt and added to a mutable list"{
+        filename = "words.txt"
+        readWordList(filename)
+        words.size shouldBe 2315
+        words[0] shouldBe "ABACK"
+    }
+    
+    "3: A random word should be picked from the list and then removed"{
+        val words = mutableListOf("bread", "thyme", "clove")
+        val word = pickRandomWord(words)
+
+        words.shouldNotContain(word)
+    
+    }
+
+    "4: The list should contain all 1's when the guess matches the target"{
+        evaluateGuess("bread", "bread") shouldBe listOf(1,1,1,1,1)
+    }
+
+    "The list should contain a mixture of 1's and 0's when partially correct"{
+        evaluateGuess("bread","beers") shouldBe listOf(1,0,1,0,0)
+    }
+
+    "The list should contain all 0's when the guess doesn't match the target in any place"{
+        evaluateGuess("bread","thyme") shouldBe listOf(0,0,0,0,0)
+    }
 })
